eee7f72724759ba728e4789c4c40a1ec554449e7
David Levayer <david.levayer@gmail.com>
2015-03-15 18:12:08 -0400

    partie AppCliente codée mais non testée

--- src/client/ApplicationClient.java
+++ src/client/ApplicationClient.java
@@ -1,22 +1,69 @@
 package client;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintStream;
 import java.net.Socket;
 
 import common.Commande;
 
 public class ApplicationClient {
-	
+
 	private int serverPort;
 	private String serverHostname;
-	
-	public ApplicationClient (Commande c, String hostname, int port){
+	private PrintStream sortieWriter;
+	private BufferedReader commandesReader;
+
+	public ApplicationClient (String hostname, int port, String entrees, String sortie){
+		
 		this.serverHostname = hostname;
 		this.serverPort = port;
-		traiteCommande(c);
+		
+		//initialise(entrees, sortie);
+		//scenario();
+		
+		String[] mArguments = new String[2];
+		mArguments[0] = "./src/server/Etudiant.java";
+		mArguments[1] = "./classes";		
+		traiteCommande(new Commande(Commande.TYPE_COMPILATION, mArguments));
+
+		
+		mArguments = new String[1];
+		mArguments[0] = "server.Etudiant";
+		traiteCommande(new Commande(Commande.TYPE_CHARGEMENT, mArguments));
+
+		mArguments = new String[2];
+		mArguments[0] = "server.Etudiant";
+		mArguments[1] = "etudiant1";
+		traiteCommande(new Commande(Commande.TYPE_CREATION, mArguments));
+
+		mArguments = new String[3];
+		mArguments[0] = "etudiant1";
+		mArguments[1] = "nom";
+		mArguments[2] = "David";
+		traiteCommande(new Commande(Commande.TYPE_ECRITURE, mArguments));
+
+		mArguments = new String[2];
+		mArguments[0] = "etudiant1";
+		mArguments[1] = "nom";
+		traiteCommande(new Commande(Commande.TYPE_LECTURE, mArguments));
+
+		mArguments = new String[3];
+		mArguments[0] = "etudiant1";
+		mArguments[1] = "isMe";
+		mArguments[2] = "java.lang.String:David";
+		traiteCommande(new Commande(Commande.TYPE_APPEL, mArguments));
+
+		mArguments = new String[3];
+		mArguments[0] = "etudiant1";
+		mArguments[1] = "isMe";
+		mArguments[2] = "java.lang.String:Bob";
+		traiteCommande(new Commande(Commande.TYPE_APPEL, mArguments));
 	}
 
 	/**
@@ -24,6 +71,58 @@ public class ApplicationClient {
 	 * variable du type Commande qui est retournée
 	 */
 	public Commande saisisCommande(BufferedReader fichier) {
+		
+		try {
+			String commandLine = fichier.readLine();
+			String[] commmandComponents = commandLine.split("#");
+			
+			Commande c = null;
+			String[] args;
+			switch(commmandComponents[0]){
+			case "compilation":
+				args = new String[2];
+				args[0] = commmandComponents[1];
+				args[1] = commmandComponents[2];
+				c = new Commande(Commande.TYPE_COMPILATION, args);
+				break;
+			case "chargement":
+				args = new String[1];
+				args[0] = commmandComponents[1];
+				c = new Commande(Commande.TYPE_CHARGEMENT, args);
+				break;
+			case "lecture":
+				args = new String[2];
+				args[0] = commmandComponents[1];
+				args[1] = commmandComponents[2];
+				c = new Commande(Commande.TYPE_LECTURE, args);
+				break;
+			case "ecriture":
+				args = new String[3];
+				args[0] = commmandComponents[1];
+				args[1] = commmandComponents[2];
+				args[2] = commmandComponents[3];
+				c = new Commande(Commande.TYPE_ECRITURE, args);
+				break;
+			case "creation":
+				args = new String[2];
+				args[0] = commmandComponents[1];
+				args[1] = commmandComponents[2];
+				c = new Commande(Commande.TYPE_CREATION, args);
+				break;
+			case "fonction":
+				args = new String[3];
+				args[0] = commmandComponents[1];
+				args[1] = commmandComponents[2];
+				args[2] = commmandComponents[3];
+				c = new Commande(Commande.TYPE_APPEL, args);
+				break;
+			}
+			
+			return c;
+			
+		} catch (IOException e) {
+			System.out.println("Erreur lors de la lecture dans "+fichier);
+		}
 		return null;
 	}
 
@@ -32,6 +131,17 @@ public class ApplicationClient {
 	 */
 	public void initialise(String fichCommandes, String fichSortie) {
 		
+		try {
+			commandesReader = new BufferedReader(new FileReader(fichCommandes));
+		} catch (FileNotFoundException e) {
+			System.out.println("Fichier "+fichCommandes+ " introuvable");
+		}
+		
+		try {
+			sortieWriter = new PrintStream(new File(fichSortie));
+		} catch (FileNotFoundException e) {
+			System.out.println("Fichier "+fichSortie+ " introuvable");
+		}
 	}
 
 	/**
@@ -42,27 +152,25 @@ public class ApplicationClient {
 	 * décrit plus haut, qui seront appelées par traiteCommande(Commande uneCommande) 
 	 */
 	public Object traiteCommande(Commande uneCommande) {
-		
+
 		try {
-			
+
 			Socket mSocket = new Socket(serverHostname, serverPort);
 			ObjectOutputStream oos = new ObjectOutputStream(mSocket.getOutputStream());
 			oos.writeObject(uneCommande);
-			//oos.close();
-			
+
 			ObjectInputStream ois = new ObjectInputStream(mSocket.getInputStream());
 			Commande c = (Commande) ois.readObject();
-			
+
 			System.out.println(c);
-			
+
 			oos.close();
 			mSocket.close();
-			
+
 			return c.getResult();
-			
+
 		} catch (IOException | ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Erreur lors du traitement de la commande");
 		} 
 		return null;
 	}
@@ -73,7 +181,15 @@ public class ApplicationClient {
 	 * traiteCommande(Commande uneCommande).
 	 */
 	public void scenario() {
-
+		sortieWriter.println("Debut des traitements:");
+		Commande prochaine = saisisCommande(commandesReader);
+		while (prochaine != null) {
+			sortieWriter.println("\tTraitement de la commande " + prochaine + " ...");
+			Object resultat = traiteCommande(prochaine);
+			sortieWriter.println("\t\tResultat: " + resultat);
+			prochaine = saisisCommande(commandesReader);
+		}
+		sortieWriter.println("Fin des traitements");
 	}
 
 	/**
@@ -86,47 +202,13 @@ public class ApplicationClient {
 	 * exécuter le scénario
 	 */
 	public static void main(String[] args) {
+
+		// TODO Extract data from args
 		
 		String hostname = "localhost";
 		int port = 1111;
+
+		new ApplicationClient(hostname, port, null, null);
 		
-		String[] mArguments = new String[2];
-		mArguments[0] = "./src/server/Etudiant.java";
-		mArguments[1] = "./classes";		
-		new ApplicationClient(new Commande(Commande.TYPE_COMPILATION, mArguments), hostname, port);
-		
-		/*
-		mArguments = new String[1];
-		mArguments[0] = "server.Etudiant";
-		new ApplicationClient(new Commande(Commande.TYPE_CHARGEMENT, mArguments), hostname, port);
-		
-		mArguments = new String[2];
-		mArguments[0] = "server.Etudiant";
-		mArguments[1] = "etudiant1";
-		new ApplicationClient(new Commande(Commande.TYPE_CREATION, mArguments), hostname, port);
-		
-		mArguments = new String[3];
-		mArguments[0] = "etudiant1";
-		mArguments[1] = "nom";
-		mArguments[2] = "David";
-		new ApplicationClient(new Commande(Commande.TYPE_ECRITURE, mArguments), hostname, port);
-		
-		mArguments = new String[2];
-		mArguments[0] = "etudiant1";
-		mArguments[1] = "nom";
-		new ApplicationClient(new Commande(Commande.TYPE_LECTURE, mArguments), hostname, port);
-		
-		mArguments = new String[3];
-		mArguments[0] = "etudiant1";
-		mArguments[1] = "isMe";
-		mArguments[2] = "java.lang.String:David";
-		new ApplicationClient(new Commande(Commande.TYPE_APPEL, mArguments), hostname, port);
-		
-		mArguments = new String[3];
-		mArguments[0] = "etudiant1";
-		mArguments[1] = "isMe";
-		mArguments[2] = "java.lang.String:Bob";
-		new ApplicationClient(new Commande(Commande.TYPE_APPEL, mArguments), hostname, port);
-		*/
 	}
 }
--- src/server/Etudiant.java
+++ src/server/Etudiant.java
@@ -12,50 +12,53 @@ import java.util.*;
 
 public class Etudiant {
 
-  private String nom;
+	private String nom;
 
-  private Vector cours;
+	private Vector cours;
 
-  public Etudiant() {
-    cours = new Vector();
-  }
+	public Etudiant() {
+		cours = new Vector();
+	}
 
-  public Etudiant(String unNom) {
-    this();
-    nom = unNom;
-  }
+	public Etudiant(String unNom) {
+		this();
+		nom = unNom;
+	}
 
-  /*
+	/*
   public void inscrisDansCours(Cours unCours) {
     cours.add(unCours);
     unCours.ajouteEtudiant(this);
   }
-  */
+	 */
 
-  public void setNom(String unNom) {
-    nom = unNom;
-  }
+	public void setNom(String unNom) {
+		nom = unNom;
+	}
 
-  public String getNom() {
-    return nom;
-  }
+	public String getNom() {
+		return nom;
+	}
 
-  public float getMoyenne() {
-    float totalNotes = 0;
-    int nombreCours = 0;
-    Iterator lesCours = cours.iterator();
-    while (lesCours.hasNext()) {
-      totalNotes = totalNotes + ((Cours)lesCours.next()).getNote(this);
-      nombreCours++;
-    }
-    return totalNotes/nombreCours;
-  }
-  
-  public boolean isMe(String nom){
-	  return this.nom.equals(nom);
-  }
+	/*
+	public float getMoyenne() {
+		float totalNotes = 0;
+		int nombreCours = 0;
+		Iterator lesCours = cours.iterator();
+		while (lesCours.hasNext()) {
+			totalNotes = totalNotes + ((Cours)lesCours.next()).getNote(this);
+			nombreCours++;
+		}
+		return totalNotes/nombreCours;
+	}
+	*/
 
-  public String toString() {
-    return nom + "[" + getMoyenne() + "]";
-  }
+	public boolean isMe(String nom){
+		return this.nom.equals(nom);
+	}
+
+	public String toString() {
+		return nom;
+		//return nom + "[" + getMoyenne() + "]";
+	}
 }
